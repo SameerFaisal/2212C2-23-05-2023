aa133865de2d5c41125d000aaf8a582642aa4691
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-23 14:23:12 +0500

    jQuery 23-05-2023 2212C2

---
+++ 23-05-2023/app.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+//     $('#btn').click(function(){
+//         var firstname = $('#first_name').val();
+//         var lastname = $('#last_name').val();
+//         var fullname=firstname+" "+lastname
+//       $('#output').html(fullname)
+       
+//     })
+// })
+// $(document).ready(function(){
+//     $('#btn').click(function(){
+//         var h=$('#hd').html()
+//         alert(h)
+       
+//     })
+// })
